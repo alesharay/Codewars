e9080b1e1f5926aa177c46a5049c9c1c90279c9b
Alesha Ray <alesha.ray@g.austincc.edu>
2018-10-14 21:14:58 -0500

    add hero kata and update readme with link

---
+++ C++/Hero.cpp
@@ -0,0 +1,12 @@
+// https://www.codewars.com/kata/is-he-gonna-survive
+
+#include <iostream>
+using namespace std;
+
+bool hero(int bullets, int dragons) {
+  return true;
+}
+
+int main(void) {
+  return 0;
+}
