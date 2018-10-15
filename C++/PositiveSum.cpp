11b0894496640402d314fe78ea60381e82f94e7e
Alesha Ray <alesha.ray@g.austincc.edu>
2018-10-15 11:50:02 -0500

    Add inital solution. All tests pass

--- C++/PositiveSum.cpp
+++ C++/PositiveSum.cpp
@@ -11,7 +11,18 @@ Note: if there is nothing to sum, the sum is default to 0.
 using namespace std;
 
 int positive_sum (const std::vector<int> arr){
-  return 0;
+  // a place to hold the sum
+  int sum = 0;
+  // for each number in the array
+  for(int i = 0; i < arr.size(); i++) {
+    // if the number is greater than 0
+    if(arr[i] > 0) {
+      // add the number to the sum
+      sum += arr[i];
+    }
+  }
+  // return the sum
+  return sum;
 }
 
 int main(int argc, char** argv) {
