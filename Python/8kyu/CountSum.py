ba619133f170aec8ecfd7f5fc58c0acfa1465e0c
Alesha Ray <alesha.ray@g.austincc.edu>
2018-10-28 13:35:42 -0500

    remove pseudocode from solution

--- Python/8kyu/CountSum.py
+++ Python/8kyu/CountSum.py
@@ -7,17 +7,11 @@ def count_positives_sum_negatives(arr):
     if arr is None or len(arr) is 0:
         return []
     else:
-        # loop through each value in the array
         for value in arr:
-            # if the value is positive
             if value > 0:
-                # increment the positive counter by 1
                 countPos += 1
-            # otherwise
             else:
-                # add the negative value to the negative sum
                 negSum += value
-    # return an array with the positive count as the first element and the negative num as the second
     return [countPos, negSum]
 
 
