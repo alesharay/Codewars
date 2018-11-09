0489e5bd77cd8349c4e1db3bac12acce22dde44e
Alesha Ray <alesha.ray@g.austincc.edu>
2018-11-08 20:25:11 -0600

    add initial solution setup for ListFilter

---
+++ Python/7kyu/ListFilter.py
@@ -0,0 +1,19 @@
+# https://www.codewars.com/kata/53dbd5315a3c69eed20002dd
+
+'''
+In this kata you will create a function that takes a list of non-negative integers and strings and returns a new list with the strings filtered out.
+'''
+
+
+def filter_list(values):
+    ''' write code here'''
+
+
+def main():
+    print(filter_list([1, 2, 'a', 'b']), [1, 2])
+    print(filter_list([1, 'a', 'b', 0, 15]), [1, 0, 15])
+    print(filter_list([1, 2, 'aasf', '1', '123', 123]), [1, 2, 123])
+
+
+if __name__ == "__main__":
+    main()
