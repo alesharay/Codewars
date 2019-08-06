ea204000f0e3ee511de058dd1df109056d38a953
Alesha Ray <alesha.ray@g.austincc.edu>
2019-08-06 13:41:22 -0500

    add stub code for EAN kata

---
+++ Java/7kyu/EAN.java
@@ -0,0 +1,17 @@
+
+
+
+public class EAN {
+    
+  public static boolean abundantNumber(int num) {
+      // Your code goes here
+      return true;
+  }
+  
+  public static void main(String[] args) {
+    System.out.println("true " + EAN.abundantNumber(12));
+    System.out.println("true " + EAN.abundantNumber(18));
+    System.out.println("false " + EAN.abundantNumber(37));
+    System.out.println("false " + EAN.abundantNumber(77));
+  }
+}
