0f116f5af8632a6094c9e4943569706acc15be18
Alesha Ray <alesha.ray@g.austincc.edu>
2019-12-26 19:55:20 -0600

    add solution to Likes kata

--- Java/6kyu/Likes.java
+++ Java/6kyu/Likes.java
@@ -11,7 +11,35 @@
 */
 
 public class Likes {
-    public static void main(String ... args) {
-        
+    public static void main(String... args) {
+        System.out.println();
+        System.out.println("no one likes this   " + Likes.whoLikesIt());
+        System.out.println("Peter likes this   " + Likes.whoLikesIt("Peter"));
+        System.out.println("Jacob and Alex like this   " + Likes.whoLikesIt("Jacob", "Alex"));
+        System.out.println("Max, John and Mark like this   " + Likes.whoLikesIt("Max", "John", "Mark"));
+        System.out.println("Alex, Jacob and 2 others like this   " + Likes.whoLikesIt("Alex", "Jacob", "Mark", "Max"));
+        System.out.println();
+    }
+
+    // public static String whoLikesIt(String... names) {
+    //     String result = "";
+    //     switch (names.length) {
+    //         case 0: result = "no one likes this"; break;
+    //         case 1: result = names[0] + " likes this"; break;
+    //         case 2: result = names[0] + " and " + names[1] + " like this"; break;
+    //         case 3: result = names[0] + ", " + names[1] + " and " + names[2] + " like this"; break;
+    //         default: result = names[0] + ", " + names[1] + " and " + (names.length - 2) + " others like this"; break;
+    //     }
+    //     return result;
+    // }
+    
+    public static String whoLikesIt(String... names) {
+        switch (names.length) {
+            case 0: return "no one likes this";
+            case 1: return String.format("%s likes this", names[0]);
+            case 2: return String.format("%s and %s like this", names[0], names[1]);
+            case 3: return String.format("%s, %s and %s like this", names[0], names[1], names[2]);
+            default: return String.format("%s, %s and %d others like this", names[0], names[1], (names.length-2));
+        }
     }
 }
