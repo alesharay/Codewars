72560fef04a606b7e884dc98e9f2b1df5c34abc5
Alesha Ray <alesha.ray@g.austincc.edu>
2019-12-30 10:39:59 -0600

    add stub code for XO kata

---
+++ Java/7kyu/XO.java
@@ -0,0 +1,28 @@
+/*
+  Check to see if a string has the same amount of 'x's and 'o's. The method must return a boolean and be case insensitive. The string can contain any char.
+
+  Examples input/output:
+
+  XO("ooxx") => true
+  XO("xooxx") => false
+  XO("ooxXm") => true
+  XO("zpzpzpp") => true // when no 'x' and 'o' is present should return true
+  XO("zzoo") => false
+*/
+
+
+public class XO {
+  
+  public static void main (String... args) {
+    System.out.println(getXO("ooxx") + " " + true);
+    System.out.println(getXO("xooxx") + " " + false);
+    System.out.println(getXO("ooxXm") + " " + true);
+    System.out.println(getXO("zpzpzpp") + " " + true); // when no 'x' and 'o' is present should return true
+    System.out.println(getXO("zzoo") + " " + false);
+  }
+
+  public static boolean getXO (String str) {
+    // Good Luck!!
+    return false;
+  }
+}
